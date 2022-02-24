da901936f7affaa9c2941c364c0032f0ecfb91fd
SirAbsolute0 <53980704+SirAbsolute0@users.noreply.github.com>
2022-02-24 01:41:01 -0600

    Full homework
    First commit with full homework finished

--- Transform/geometric.py
+++ Transform/geometric.py
@@ -1,5 +1,6 @@
+from threading import local
 from .interpolation import interpolation
-
+import numpy as np
 class Geometric:
     def __init__(self):
         pass
@@ -9,8 +10,50 @@ class Geometric:
                 image: input image
                 theta: angle to rotate the image by (in radians)
                 return the rotated image"""
+        local_image = image.copy()
+        local_theta = theta
+        rotation = np.array(( (np.cos(local_theta), -np.sin(local_theta)), 
+                                (np.sin(local_theta), np.cos(local_theta))))
 
-        return image
+        top_left = np.array((0,0)) * rotation
+        top_right = np.array((0, local_image.shape[1])) * rotation
+        bottom_left = np.array((local_image.shape[0], 0)) * rotation
+        bottom_right = np.array((local_image.shape[0], local_image.shape[1])) * rotation
+
+        first_point = top_left.sum(axis = 1)
+        second_point = top_right.sum(axis = 1)
+        third_point = bottom_left.sum(axis = 1)
+        fourth_point = bottom_right.sum(axis = 1)
+
+        max_x = 0
+        max_y = 0
+        min_x = 0
+        min_y = 0
+
+        four_points = [first_point, second_point, third_point, fourth_point]
+        for point in four_points:
+            if (point[0] < min_x):
+                min_x = point[0]
+            if (point[0] > max_x):
+                max_x = point[0]
+            if (point[1] < min_y):
+                min_y = point[1]
+            if (point[1] > max_y):
+                max_y = point[1]
+        
+        rows = int(max_x - min_x)
+        cols = int(max_y - min_y)
+
+        new_image = np.zeros((rows, cols))
+
+        for y in range(0, local_image.shape[1]):
+            for x in range(0, local_image.shape[0]):
+                point = np.array((x, y)) * rotation
+                coordinate = point.sum(axis = 1)
+
+                new_image[int(coordinate[0] - min_x), int(coordinate[1] - min_y)] = local_image[x, y]
+
+        return new_image
 
     def reverse_rotation(self, rotated_image, theta, origin, original_shape):
         """Computes the reverse rotated image by an angle theta
@@ -20,7 +63,23 @@ class Geometric:
                 Original shape: shape of the orginal image
                 return the original image"""
 
-        return rotated_image
+        local_image = rotated_image.copy()
+        local_theta = theta * -1
+        rotation = np.array(( (np.cos(local_theta), -np.sin(local_theta)), 
+                                (np.sin(local_theta), np.cos(local_theta))))
+
+        new_image = np.zeros(original_shape)
+        
+        for y in range(0, local_image.shape[1]):
+            for x in range (0, local_image.shape[0]):
+                temp = np.array((x, y)) - origin
+                point = temp * rotation
+                coordinate = point.sum(axis = 1)
+
+                if( (0 <= coordinate[0] < original_shape[0]) and (0 <= coordinate[1] < original_shape[1]) ):
+                    new_image[int(coordinate[0]), int(coordinate[1])] = local_image[x, y]
+
+        return new_image
 
     def rotate(self, image, theta, interpolation_type):
         """Computes the forward rotated image by an angle theta using interpolation
@@ -29,6 +88,76 @@ class Geometric:
                 interpolation_type: type of interpolation to use (nearest_neighbor, bilinear)
                 return the original image"""
 
-        return image
+        local_image = image.copy()
+        local_theta = theta
+        reverse_theta = local_theta * -1
+
+        rotation = np.array(( (np.cos(local_theta), -np.sin(local_theta)), 
+                                (np.sin(local_theta), np.cos(local_theta))))
+        
+        reverse_rotation = np.array(( (np.cos(reverse_theta), -np.sin(reverse_theta)), 
+                                        (np.sin(reverse_theta), np.cos(reverse_theta))))
+
+        top_left = np.array((0,0)) * rotation
+        top_right = np.array((0, local_image.shape[1])) * rotation
+        bottom_left = np.array((local_image.shape[0], 0)) * rotation
+        bottom_right = np.array((local_image.shape[0], local_image.shape[1])) * rotation
+
+        first_point = top_left.sum(axis = 1)
+        second_point = top_right.sum(axis = 1)
+        third_point = bottom_left.sum(axis = 1)
+        fourth_point = bottom_right.sum(axis = 1)
+
+        max_x = 0
+        max_y = 0
+        min_x = 0
+        min_y = 0
+
+        four_points = [first_point, second_point, third_point, fourth_point]
+        for point in four_points:
+            if (point[0] < min_x):
+                min_x = point[0]
+            if (point[0] > max_x):
+                max_x = point[0]
+            if (point[1] < min_y):
+                min_y = point[1]
+            if (point[1] > max_y):
+                max_y = point[1]
+        
+        rows = int(max_x - min_x)
+        cols = int(max_y - min_y)
+
+        origin = np.array((-min_x, -min_y))
+        
+        new_image = np.zeros((rows, cols))
+
+        for y in range(0, local_image.shape[1]):
+            for x in range(0, local_image.shape[0]):
+                point = np.array((x, y)) * rotation
+                coordinate = point.sum(axis = 1)
+
+                new_image[int(coordinate[0] - min_x), int(coordinate[1] - min_y)] = local_image[x, y]
+
+        for y in range(0, new_image.shape[1]):
+            for x in range(0, new_image.shape[0]):
+                coordinate = np.array((x, y))
+                location_origin = np.array(coordinate - origin)
+
+                inverse_location = location_origin * reverse_rotation
+                inverse_coordinate = inverse_location.sum(axis = 1)
+
+                if (interpolation_type == "nearest_neighbor"):
+                    neighbor = np.array((round(inverse_coordinate[0], 0), round(inverse_coordinate[1], 0)))
+
+                    if ( (0 <= neighbor[0] < local_image.shape[0]) and (0 <= neighbor[1] < local_image.shape[1]) ):
+                        new_image[x, y] = local_image[int(neighbor[0]), int(neighbor[1])]
+
+                elif (interpolation_type == "bilinear"):
+                    if ( 0 < inverse_coordinate[0] < local_image.shape[0] - 1 and 0 < inverse_coordinate[1] < local_image.shape[1] - 1):
+                        intensity = interpolation.bilinear_interpolation(inverse_coordinate, local_image)
+
+                        new_image[x, y] = intensity
+                    
+        return new_image
 
 
--- Transform/interpolation.py
+++ Transform/interpolation.py
@@ -1,6 +1,8 @@
+import math
+import numpy as np
 class interpolation:
 
-    def linear_interpolation(self):
+    def linear_interpolation(point, point1, point2, i1, i2):
         """Computes the linear interpolation value at some iD location x between two 1D points (Pt1 and Pt2).
         
         There are no arguments defined in the function definition on purpose. It is left upto the student to define any requierd arguments.
@@ -11,9 +13,10 @@ class interpolation:
 
         # Write your code for linear interpolation here
 
-        return
+        i = ((i1*(point2 - point))/(point2 - point1)) + ((i2*(point - point1))/(point2 - point1))
+        return i
 
-    def bilinear_interpolation(self):
+    def bilinear_interpolation(point, image):
 
         """Computes the bi linear interpolation value at some 2D location x between four 2D points (Pt1, Pt2, Pt3, and Pt4).
         
@@ -26,6 +29,18 @@ class interpolation:
         # Write your code for bilinear interpolation here
         # Recall that bilinear interpolation performs linear interpolation three times
         # Please reuse or call linear interpolation method three times by passing the appropriate parameters to compute this task
+        first_point = np.array((math.floor(point[0]), math.floor(point[1])))
+        second_point = np.array((math.floor(point[0]), math.ceil(point[1])))
+        third_point = np.array((math.ceil(point[0]), math.floor(point[1])))
+        fourth_point = np.array((math.ceil(point[0]), math.ceil(point[1])))
+        
+        i1 = image[first_point[0]][first_point[1]]
+        i2 = image[second_point[0]][second_point[1]]
+        i3 = image[third_point[0]][third_point[1]]
+        i4 = image[fourth_point[0]][fourth_point[1]]
 
-        return
+        iR1 = interpolation.linear_interpolation(point[1],first_point[1], second_point[1], i1, i2)
+        iR2 = interpolation.linear_interpolation(point[1],third_point[1], fourth_point[1], i3, i4)
+        intensity = interpolation.linear_interpolation(point[0], first_point[0], third_point[0], iR1, iR2)
+        return intensity
 
